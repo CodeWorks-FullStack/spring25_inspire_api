ab1650c730c0d1abae327f2eceb5438f415f9d89
Jeremy Fowler <jembyfowler@gmail.com>
2025-03-13 09:44:43 -0600

    yeeted the values stuff

--- src/db/DbContext.js
+++ src/db/DbContext.js
@@ -1,6 +1,5 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account.js'
-import { ValueSchema } from '../models/Value.js'
 import { QuoteSchema } from '../models/Quote.js';
 
 class DbContext {
